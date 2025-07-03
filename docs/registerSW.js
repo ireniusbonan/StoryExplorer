2a5d8aaebded458620cd074e34d0d3541f857d1f
Irenius Bonan <ireniusbonan@gmail.com>
2025-07-04 02:36:05 +0700

    Deploy to GitHub Pages

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/StoryExplorer/sw.js', { scope: '/StoryExplorer/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/StoryExplorer/service-worker.js', { scope: '/StoryExplorer/' })})}
