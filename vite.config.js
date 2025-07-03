7405405bf876c91305a9995300ce286634910dd4
Irenius Bonan <ireniusbonan@gmail.com>
2025-07-04 02:51:29 +0700

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,19 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/StoryExplorer/", // Ganti sesuai subfolder GitHub Pages jika perlu
+  base: "/StoryExplorer/",
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
-      strategies: "injectManifest", // Agar kamu bisa kontrol isi service-worker.js manual
-      srcDir: "src", // Folder tempat SW berada
-      filename: "service-worker.js", // File service worker kamu
-      injectRegister: "auto", // Otomatis daftarkan SW di index.html
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "service-worker.js",
+      injectRegister: "auto",
       manifest: {
         name: "Story Explorer",
         short_name: "StoryApp",
         description: "Aplikasi eksplorasi cerita berbasis lokasi",
-        start_url: "/StoryExplorer/", // HARUS sesuaikan dengan base
+        start_url: "/StoryExplorer/",
         display: "standalone",
         background_color: "#ffffff",
         theme_color: "#42a5f5",
@@ -24,19 +24,11 @@ export default defineConfig({
             src: "icons/icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
-            purpose: "any",
           },
           {
             src: "icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "icons/icon-144x144.png",
-            sizes: "144x144",
-            type: "image/png",
-            purpose: "any",
           },
           {
             src: "icons/icon-maskable-192x192.png",
@@ -51,10 +43,9 @@ export default defineConfig({
             purpose: "maskable",
           },
         ],
-        prefer_related_applications: false,
       },
       devOptions: {
-        enabled: true, // Aktifkan PWA saat `vite` dijalankan (dev mode)
+        enabled: true,
         type: "module",
       },
     }),
